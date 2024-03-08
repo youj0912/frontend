7c16bf1761d83ea03d03b433e6b3e4227e2d11b3
sunjom <saysaysay223@naver.com>
2024-03-08 16:47:57 +0900

    docs : [FE] 필요없는 console.log 제거

--- src/module/SizeRefactor.js
+++ src/module/SizeRefactor.js
@@ -8,13 +8,10 @@ export const getResizeEventListener = (standardWidth, standardHeight) =>{
         app.style.height = `${standardHeight}px`;
         let width = root.clientWidth;
         let height = width * (standardHeight / standardWidth);
-        console.log(height);
         if(height < 950){
             app.style.zoom =0.9;
         }else{
             app.style.zoom = height / standardHeight;
         }
-        console.log(app.style.zoom);
-
     }
 }
